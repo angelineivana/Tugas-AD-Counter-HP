df41e646f63c2fa42380af15a79b13200fe362a3
angelineivana <angelineivana01@student.ciputra.ac.id>
2022-06-08 10:08:45 +0700

    test

--- Tugas AD Counter HP/Form2.cs
+++ Tugas AD Counter HP/Form2.cs
@@ -30,31 +30,49 @@ namespace Tugas_AD_Counter_HP
         int price = 0;
         private void FormInput_Load(object sender, EventArgs e)
         {
+            //buat 5 kolom dtShowProd2 (dtshowprod2 buat source data dgvPrintProduct2)
             dtShowProd2.Columns.Add("No");
             dtShowProd2.Columns.Add("NamaProduk");
             dtShowProd2.Columns.Add("Kuantitas");
             dtShowProd2.Columns.Add("Harga");
             dtShowProd2.Columns.Add("Jumlah");
             dgvPrintProduct2.DataSource = dtShowProd2;
+
+            nudQuan.Enabled = false;
         }
         string currentPrice;
         private void comboBoxProdName_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //dtDetailProd diclear setiap cbox nama produk diubah karena dibuat supaya berisi 1 row data saja (per nama produk)
             dtDetailProd.Clear();
+
+            //product id buat textboxProdID, product price buat textboxProdPrice, select ketika namanya sama dengan cboxprodname
             sqlQuery = "SELECT product_id as 'Product ID', product_price as 'Product Price' from PRODUCT where product_name = '" + comboBoxProdName.SelectedItem.ToString() + "'";
             sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
             sqlAdapter = new MySqlDataAdapter(sqlCommand);
             sqlAdapter.Fill(dtDetailProd);
 
+            //row ke 0 karena dtDetailProd berisi 1 row saja dan kolom sesuai dengan query
             textBoxProdID.Text = dtDetailProd.Rows[0][0].ToString();
-            currentPrice = dtDetailProd.Rows[0][1].ToString();
             textBoxProdPrice.Text = "Rp " + dtDetailProd.Rows[0][1].ToString();
+
+            //buat variable baru bernama currentPrice untuk mengambil value harga per product yang blm dihitung
+            currentPrice = dtDetailProd.Rows[0][1].ToString();
+
+            nudQuan.Enabled = true;
         }
         private void nudQuan_ValueChanged(object sender, EventArgs e)
         {
-            price = Convert.ToInt32(currentPrice);
-            subtotal = Convert.ToInt32(nudQuan.Value) * price;
-            labelDisSubTotal.Text = subtotal.ToString();
+            if (comboBoxProdName.Text == "")
+            {
+                nudQuan.Enabled = false;
+            }
+            else
+            {
+                price = Convert.ToInt32(currentPrice);
+                subtotal = Convert.ToInt32(nudQuan.Value) * price;
+                labelDisSubTotal.Text = subtotal.ToString();
+            }
         }
         int no = 0;
         int subtotalAll = 0;
@@ -62,14 +80,13 @@ namespace Tugas_AD_Counter_HP
 
         private void buttonAddProd_Click(object sender, EventArgs e)
         {
-            currentPrice = "0";
             if (nudQuan.Value == 0)
             {
                 MessageBox.Show("Kuantitas Harus Lebih Dari 0");
             }
             else if (comboBoxProdName.Text == "")
             {
-                MessageBox.Show("Produk Belum Dipilih!");
+                MessageBox.Show("Produk Belum Dipilih");
             }
             else
             {
@@ -124,6 +141,8 @@ namespace Tugas_AD_Counter_HP
             if (dtPromo.Rows.Count == 0)
             {
                 MessageBox.Show("Promo Tidak Berlaku");
+                comboBoxPromoName.ResetText();
+                comboBoxPromoName.SelectedItem = null;
             }
             else
             {
