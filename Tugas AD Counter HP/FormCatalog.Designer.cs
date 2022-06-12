51b15f484bc20997a668d2d3bceb333c2627306b
Pend12 <95454604+Pend12@users.noreply.github.com>
2022-06-12 23:10:40 +0700

    Catalog dan membenarkan tombol exit (#1)

--- Tugas AD Counter HP/FormCatalog.Designer.cs
+++ Tugas AD Counter HP/FormCatalog.Designer.cs
@@ -30,32 +30,81 @@ namespace Tugas_AD_Counter_HP
         private void InitializeComponent()
         {
             this.buttonMenu = new System.Windows.Forms.Button();
+            this.dataGridViewCatalog = new System.Windows.Forms.DataGridView();
+            this.richTextBoxSpesifikasi = new System.Windows.Forms.RichTextBox();
+            this.labelSpesifikasi = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCatalog)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonMenu
             // 
-            this.buttonMenu.Location = new System.Drawing.Point(55, 350);
+            this.buttonMenu.Location = new System.Drawing.Point(939, 520);
+            this.buttonMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonMenu.Name = "buttonMenu";
-            this.buttonMenu.Size = new System.Drawing.Size(145, 50);
+            this.buttonMenu.Size = new System.Drawing.Size(105, 44);
             this.buttonMenu.TabIndex = 1;
             this.buttonMenu.Text = "Back To Menu";
             this.buttonMenu.UseVisualStyleBackColor = true;
             this.buttonMenu.Click += new System.EventHandler(this.buttonMenu_Click);
             // 
+            // dataGridViewCatalog
+            // 
+            this.dataGridViewCatalog.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewCatalog.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridViewCatalog.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridViewCatalog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCatalog.Location = new System.Drawing.Point(3, 30);
+            this.dataGridViewCatalog.Name = "dataGridViewCatalog";
+            this.dataGridViewCatalog.RowHeadersVisible = false;
+            this.dataGridViewCatalog.RowHeadersWidth = 51;
+            this.dataGridViewCatalog.RowTemplate.Height = 24;
+            this.dataGridViewCatalog.Size = new System.Drawing.Size(617, 534);
+            this.dataGridViewCatalog.TabIndex = 2;
+            this.dataGridViewCatalog.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewCatalog_CellClick);
+            this.dataGridViewCatalog.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewCatalog_CellContentClick);
+            // 
+            // richTextBoxSpesifikasi
+            // 
+            this.richTextBoxSpesifikasi.Location = new System.Drawing.Point(626, 30);
+            this.richTextBoxSpesifikasi.Name = "richTextBoxSpesifikasi";
+            this.richTextBoxSpesifikasi.Size = new System.Drawing.Size(418, 485);
+            this.richTextBoxSpesifikasi.TabIndex = 3;
+            this.richTextBoxSpesifikasi.Text = "";
+            // 
+            // labelSpesifikasi
+            // 
+            this.labelSpesifikasi.AutoSize = true;
+            this.labelSpesifikasi.Location = new System.Drawing.Point(969, 9);
+            this.labelSpesifikasi.Name = "labelSpesifikasi";
+            this.labelSpesifikasi.Size = new System.Drawing.Size(75, 17);
+            this.labelSpesifikasi.TabIndex = 4;
+            this.labelSpesifikasi.Text = "Spesifikasi";
+            // 
             // FormCatalog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1056, 575);
+            this.Controls.Add(this.labelSpesifikasi);
+            this.Controls.Add(this.richTextBoxSpesifikasi);
+            this.Controls.Add(this.dataGridViewCatalog);
             this.Controls.Add(this.buttonMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormCatalog";
-            this.Text = "FormCatalog";
+            this.Text = "Catalog";
+            this.Load += new System.EventHandler(this.FormCatalog_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCatalog)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonMenu;
+        private System.Windows.Forms.DataGridView dataGridViewCatalog;
+        private System.Windows.Forms.RichTextBox richTextBoxSpesifikasi;
+        private System.Windows.Forms.Label labelSpesifikasi;
     }
 }
--- Tugas AD Counter HP/FormCatalog.cs
+++ Tugas AD Counter HP/FormCatalog.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Tugas_AD_Counter_HP
 {
@@ -16,12 +17,39 @@ namespace Tugas_AD_Counter_HP
         {
             InitializeComponent();
         }
-
+        public static string sqlconnection = "server=139.255.11.84;uid=student;pwd=isbmantap;database=DBD_02_PHONESTORE";
+        public MySqlConnection sqlConnect = new MySqlConnection(sqlconnection);
+        public MySqlCommand sqlCommand;
+        public MySqlDataAdapter sqlAdapter;
+        public string sqlQuery;
+        DataTable produk = new DataTable();
         private void buttonMenu_Click(object sender, EventArgs e)
         {
             FormMenu formMenu = new FormMenu();
             this.Hide();
             formMenu.Show();
+        }
+
+        private void FormCatalog_Load(object sender, EventArgs e)
+        {
+            sqlQuery = "select PRODUCT_BRAND as 'Brand',PRODUCT_NAME as 'Nama Produk',PRODUCT_PRICE as 'Harga',PRODUCT_STOCK as 'Stok',PRODUCT_DESC as 'Spesifikasi' from PRODUCT;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(produk);
+            dataGridViewCatalog.DataSource = produk;
+        }
+
+        private void dataGridViewCatalog_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void dataGridViewCatalog_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1 )
+            {
+                DataGridViewRow baris = dataGridViewCatalog.Rows[e.RowIndex];
+                richTextBoxSpesifikasi.Text = baris.Cells[4].Value.ToString();
+            }
         }
     }
 }
