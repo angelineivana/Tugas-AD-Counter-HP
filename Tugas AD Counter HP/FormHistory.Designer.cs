de2471cab2b8e52a5acc2f3ee871c612951242d9
P1X3L07 <patricialowis2@gmail.com>
2022-06-15 20:54:54 +0700

    history

--- Tugas AD Counter HP/FormHistory.Designer.cs
+++ Tugas AD Counter HP/FormHistory.Designer.cs
@@ -31,11 +31,17 @@ namespace Tugas_AD_Counter_HP
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHistory));
             this.buttonMenu = new System.Windows.Forms.Button();
+            this.textBoxCustomer = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.dGVHistory = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVHistory)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonMenu
             // 
-            this.buttonMenu.Location = new System.Drawing.Point(49, 344);
+            this.buttonMenu.Location = new System.Drawing.Point(36, 453);
             this.buttonMenu.Name = "buttonMenu";
             this.buttonMenu.Size = new System.Drawing.Size(145, 50);
             this.buttonMenu.TabIndex = 1;
@@ -43,22 +49,89 @@ namespace Tugas_AD_Counter_HP
             this.buttonMenu.UseVisualStyleBackColor = true;
             this.buttonMenu.Click += new System.EventHandler(this.buttonMenu_Click);
             // 
+            // textBoxCustomer
+            // 
+            this.textBoxCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCustomer.Location = new System.Drawing.Point(36, 76);
+            this.textBoxCustomer.Name = "textBoxCustomer";
+            this.textBoxCustomer.Size = new System.Drawing.Size(518, 30);
+            this.textBoxCustomer.TabIndex = 2;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.Color.Blue;
+            this.buttonSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSearch.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonSearch.Location = new System.Drawing.Point(580, 71);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 35);
+            this.buttonSearch.TabIndex = 3;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.Color.Firebrick;
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonClear.Location = new System.Drawing.Point(673, 71);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 35);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // dGVHistory
+            // 
+            this.dGVHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVHistory.Location = new System.Drawing.Point(36, 127);
+            this.dGVHistory.Name = "dGVHistory";
+            this.dGVHistory.RowHeadersWidth = 62;
+            this.dGVHistory.RowTemplate.Height = 28;
+            this.dGVHistory.Size = new System.Drawing.Size(712, 304);
+            this.dGVHistory.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Search :";
+            // 
             // FormHistory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 533);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dGVHistory);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxCustomer);
             this.Controls.Add(this.buttonMenu);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormHistory";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormHistory";
+            this.Load += new System.EventHandler(this.FormHistory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dGVHistory)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonMenu;
+        private System.Windows.Forms.TextBox textBoxCustomer;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.DataGridView dGVHistory;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Tugas AD Counter HP/FormHistory.cs
+++ Tugas AD Counter HP/FormHistory.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Tugas_AD_Counter_HP
 {
@@ -16,12 +17,52 @@ namespace Tugas_AD_Counter_HP
         {
             InitializeComponent();
         }
+        public static string sqlconnection = "server=139.255.11.84;uid=student;pwd=isbmantap;database=DBD_02_PHONESTORE";
+        public MySqlConnection sqlConnect = new MySqlConnection(sqlconnection);
+        public MySqlCommand sqlCommand;
+        public MySqlDataAdapter sqlAdapter;
+        string sqlQuery;
+        DataTable dtInvoice = new DataTable();
+        DataTable dtCusName = new DataTable();
+        public static string custName;
 
         private void buttonMenu_Click(object sender, EventArgs e)
         {
             FormMenu formMenu = new FormMenu();
             this.Hide();
             formMenu.Show();
+        }
+
+        private void FormHistory_Load(object sender, EventArgs e)
+        {
+            sqlQuery = "select date_format(i.INV_DATE, '%d-%m-%y') as 'Tanggal', c.CUST_NAME, c.CUST_PHONE, p.INV_NO, p.PRODUCT_ID, i.PROMO_ID, concat('Rp. ', p.INV_PRICE) as Total, e.EMP_NAME from PRODUCT_INVOICE p, INVOICE i, CUSTOMER c, EMPLOYEE e WHERE i.CUST_ID = c.CUST_ID and i.INV_NO = p.INV_NO and e.EMP_ID = i.EMP_ID group by c.CUST_NAME;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtInvoice);
+            dGVHistory.DataSource = dtInvoice;
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            sqlQuery = "select date_format(i.INV_DATE, '%d-%m-%y') as 'Tanggal', c.CUST_NAME, c.CUST_PHONE, p.INV_NO, p.PRODUCT_ID, i.PROMO_ID, concat('Rp. ', p.INV_PRICE) as Total, e.EMP_NAME from PRODUCT_INVOICE p, INVOICE i, CUSTOMER c, EMPLOYEE e WHERE i.CUST_ID = c.CUST_ID and i.INV_NO = p.INV_NO and e.EMP_ID = i.EMP_ID and c.CUST_NAME like '%" + textBoxCustomer.Text + "%' or c.CUST_PHONE = '%" + textBoxCustomer.Text + "%' group by c.CUST_NAME ;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtCusName);
+            dGVHistory.DataSource = dtCusName;
+            if (dGVHistory.Rows.Count > 0)
+            {
+                custName = dtCusName.Rows[0][0].ToString();
+            }
+            else
+            {
+                MessageBox.Show("ulangi");
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            FormHistory_Load(sender, e);
+            textBoxCustomer.Text = "";
         }
     }
 }
