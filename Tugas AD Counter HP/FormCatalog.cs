742d62bed0b09614496e75c481556325591fe0b0
angelineivana <angelineivana01@student.ciputra.ac.id>
2022-06-16 11:23:58 +0700

    keterangan form catalog

--- Tugas AD Counter HP/FormCatalog.cs
+++ Tugas AD Counter HP/FormCatalog.cs
@@ -35,6 +35,7 @@ namespace Tugas_AD_Counter_HP
             sqlAdapter.Fill(produk);
             dataGridViewCatalog.DataSource = produk;
 
+            //awalnya di disable semua
             comboBoxCat.Enabled = false;
             comboBoxBrand.Enabled = false;
             checkBoxCat.Checked = false;
@@ -43,21 +44,28 @@ namespace Tugas_AD_Counter_HP
 
         private void dataGridViewCatalog_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            //setiap cell diclick, dt foto produk diclear
             fotoProduk.Clear();
+
             if (e.RowIndex != -1)
             {
                 DataGridViewRow baris = dataGridViewCatalog.Rows[e.RowIndex];
                 richTextBoxSpesifikasi.Text = baris.Cells[4].Value.ToString();
 
+                //query buat ambil image url dr tabel PRODUCT selama nama produk di PRODUCT sama dengan yg dipilih
                 sqlQuery = "select PRODUCT_IMAGE_URL from PRODUCT where '" + baris.Cells[1].Value.ToString() +"' = PRODUCT_NAME";
                 sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
                 sqlAdapter = new MySqlDataAdapter(sqlCommand);
                 sqlAdapter.Fill(fotoProduk);
+
+                //buat sumber pictureBoxProduct
                 pictureBoxProduct.ImageLocation = fotoProduk.Rows[0][0].ToString();
             }
         }
+        //buat nentuin datasource dr datagridviewcatalog
         private void dataSource()
         {
+            //buat logika filter by kategori dan brand
             if (checkBoxBrand.Checked == false && checkBoxCat.Checked == false)
             { 
                 dataGridViewCatalog.DataSource = produk;
@@ -77,9 +85,13 @@ namespace Tugas_AD_Counter_HP
         }
         private void fillKategori()
         {
+            //selama ada item yg dipilih di combobox category
             if (comboBoxCat.SelectedIndex != -1)
             {
+                //dt kategori diclear setiap isi dt
                 kategori.Clear();
+
+                //query buat isi dt kategori
                 sqlQuery = "select P.PRODUCT_BRAND as 'Brand', P.PRODUCT_NAME as 'Nama Produk', P.PRODUCT_PRICE as 'Harga', P.PRODUCT_STOCK as 'Stok', P.PRODUCT_DESC as 'Spesifikasi' from PRODUCT P, CATEGORY C where P.CAT_ID = C.CAT_ID and C.CAT_NAME = '" + comboBoxCat.SelectedItem.ToString() + "'";
                 sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
                 sqlAdapter = new MySqlDataAdapter(sqlCommand);
@@ -111,8 +123,11 @@ namespace Tugas_AD_Counter_HP
         }
         private void checkBoxCat_CheckedChanged(object sender, EventArgs e)
         {
+            //reset comboboxcat
             comboBoxCat.Text = null;
             comboBoxCat.SelectedIndex = -1;
+
+            //jika checkbox di cawang, combobox dibuka & sebaliknya
             if (checkBoxCat.Checked == true)
             {
                 comboBoxCat.Enabled = true;
@@ -124,11 +139,16 @@ namespace Tugas_AD_Counter_HP
         }
         private void comboBoxCat_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //isi dt kategori
             fillKategori();
+
+            //jika dua checkbox dicawang, dt katbrand diisi
             if (checkBoxBrand.Checked == true && checkBoxCat.Checked == true)
             {
                 fillKatBrand();
             }
+
+            //nentuin datasource
             dataSource();
         }
 
